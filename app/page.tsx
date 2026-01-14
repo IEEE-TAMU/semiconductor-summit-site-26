2a934eda49d44341a973ee94d2a011430e7869ce
Jadon Lee <jadonlee888@gmail.com>
2026-01-13 22:30:46 -0600

    todos

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,18 @@ import Sponsors from '@/components/Sponsors';
 import Organizers from '@/components/Organizers';
 import Footer from '@/components/Footer';
 
+/**
+ * 
+ * TODO LIST:
+ * - add location information to about
+ * - combine mission and about sections?
+ * - page for those interested in sponsoring. collect their contact info and provide the sponsorship packet
+ * - button for sign up to speak?
+ * - more writing for the different sections in about
+ * - write that the event is free and open to all somewhere
+ * - some link to the IEEE TAMU website
+ */
+
 export default function Home() {
   return (
     <main className="min-h-screen">
